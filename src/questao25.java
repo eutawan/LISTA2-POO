fa7556630969def4ccb9ac32234756938157d36b
eutawan <principalacc169@gmail.com>
2023-08-31 01:25:19 -0300

    questao25
    minha vigésima quinta questão

---
+++ src/questao25.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class questao25 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int numero2 = scanner.nextInt();
+
+        System.out.print("Digite o terceiro número: ");
+        int numero3 = scanner.nextInt();
+
+        int maior = Math.max(numero1, Math.max(numero2, numero3));
+        int menor = Math.min(numero1, Math.min(numero2, numero3));
+
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
+    }
+}
+
