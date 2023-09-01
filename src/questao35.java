3e5d663c1dd0e1e7a7cc9ddfadf54e1680c47966
eutawan <principalacc169@gmail.com>
2023-09-01 14:46:32 -0300

    questao35
    minha trigésima quinta questão

---
+++ src/questao35.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class questao35 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de termos: ");
+        int N = scan.nextInt();
+        scan.close();
+
+        for (int i = 1; i < N + 1; i++) {
+            for (int a = 1; a < i + 1; a++) {
+
+                System.out.print(a);
+
+            }
+            System.out.println(" ");
+        }
+    }
+}
